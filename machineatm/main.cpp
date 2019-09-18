86d164e6b5805a808b0ebf0e743ee1d4e779b0bd
Abbie Thorne <thorneal@vcu.edu>
2019-09-18 16:31:25 -0400

    Initial Commit

---
+++ machineatm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  machineatm
+//
+//  Created by Abbie Thorne on 9/18/19.
+//  Copyright © 2019 Abbie Thorne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
